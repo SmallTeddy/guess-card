c3170f9565f3a30cefe23dc606ff20ccac599428
SmallTeddy <zhangzeming1212@gmail.com>
2024-03-15 10:47:00 +0800

    feat: add card assets

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
-  <div h-screen flex-center font-size-30 color-gray>
-    游戏字体测试
+  <div h-screen flex-center color-gray>
+    <div v-for="num in 10" :key="num">
+      <div v-for="col in ['黑桃', '红桃', '梅花', '方块']" :key="col">
+        <img :src="getCard(col as CardType, num as CardNum)" alt="黑桃1">
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang='ts' setup>
+import { getAssetsFile } from '@/utils/index'
 
+type CardType = '黑桃' | '红桃' | '梅花' | '方块'
+type CardNum = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+type CardColor = 'S' | 'H' | 'C' | 'D'
+const cardMap: Record<CardType, CardColor> = {
+  '黑桃': 'S',
+  '红桃': 'H',
+  '梅花': 'C',
+  '方块': 'D',
+}
+
+const getCard = (cardType: CardType, num: CardNum): string => {
+  const card = cardMap[cardType] + num
+  const cardAsset = `card/${card}.png`
+  return getAssetsFile(cardAsset)
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 获取assets静态资源，路径为assets
+ * @param url 图片名称
+ * @returns 
+ */
+export const getAssetsFile = (url: string): string => {
+  return new URL(`../assets/${url}`, import.meta.url).href
+}
